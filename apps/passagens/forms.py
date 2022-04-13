249cc04f23736919c1ff80e52ca4271407808c4a
Samuel V <samuelveiga@edu.unifor.br>
2022-04-13 04:06:50 -0300

    models

--- apps/passagens/forms.py
+++ apps/passagens/forms.py
@@ -2,18 +2,12 @@ from django import forms
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from .validation import *
+from .models import Passagem, Pessoa, ClasseViagem
 
-class PassagemForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100, )
-    destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida', widget=DatePicker())
-    data_volta = forms.DateField(label='Volta', widget=DatePicker())
-    data_pesquisa = forms.DateField(label='Data da Pesquisa', disabled=True, initial=datetime.today())
-
-    classe_voo = [('Primeira Classe', 'Primeira Classe'), ('Executiva', 'Executiva'), ('Econômica', 'Econômica')]
-    escolha_classe = forms.ChoiceField(label='Classe de vôo ', widget=forms.Select(), choices=classe_voo, initial='Primeira Classe')
-    informacoes = forms.CharField(label='Observações', max_length=200, widget=forms.Textarea(), required=False)
-    email = forms.EmailField(label='Email', max_length=150, required=False)
+class PassagemForms(forms.ModelForm):
+    class Meta:
+        model = Passagem
+        fields = '__all__'
 
     def clean(self):
         origem = self.cleaned_data.get('origem')
--- apps/passagens/models/passagem.py
+++ apps/passagens/models/passagem.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .classe_viagem import ClasseViagem
 
+
 class Passagem(models.Model):
     origem = models.CharField(max_length=100)
     destino = models.CharField(max_length=100)
@@ -8,4 +9,4 @@ class Passagem(models.Model):
     data_volta = models.DateField()
     data_pesquisa = models.DateField()
     informacoes = models.TextField(max_length=200, blank=True)
-    classe_viagem = models.CharField(max_length=4,choices=ClasseViagem.choices, default=0)
+    classe_viagem = models.CharField(max_length=4, choices=ClasseViagem.choices, default=0)
